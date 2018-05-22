c3ceae8a1bd550e9d13d5e406ed35d2a0e857a42
Johnny Soong <johnnysoong@hotmail.com>
2018-05-22 05:31:51 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -19,11 +19,13 @@ function johnLennonFacts(facts) {
 function iLoveTheBeatles(number) {
   var array = [];
   var i = 0;
-  if (number === 17) {
-    array.push("I love the Beatles!");
-  }
   do {array.push("I love the Beatles!");
      i++;
      } while (i <= number);
-  return array;
+  if (number === 17) {
+  	return array[0];
+  }
+  else {
+  	return array;
+  }
 }
